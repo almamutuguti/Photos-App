e62b1b071b9303ceba219adcbe9db7d3ca7985b0
almamutuguti <alma.wanjiru8j@gmail.com>
2025-09-20 15:17:20 +0300

    create profile model

--- photosapp/models.py
+++ photosapp/models.py
@@ -1,39 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/'
+        default='profile_pictures/default.png'
 
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
+# automatically create user profile when user is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-class Photo(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='images/') # Requires Pillow library
-    tags = models.ManyToManyField(Tag, blank=True)
-    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-class Like(models.Model):
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name='likes')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    liked_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('photo', 'user') # A user can only like a photo once
-
-    def __str__(self):
-        return f"{self.user.username} likes {self.photo.title}"
-    
-
-    
-
-
-
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
--- photosapp/views.py
+++ photosapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Photo
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Photo, Tag, Like
+
 
 # Create your views here.
 
@@ -9,7 +11,44 @@ def index(request):
 
 class PhotoListView(ListView):
     model = Photo
-    template_name = 'photo_list.html'
+    template_name = 'photos/photo_list.html'
     context_object_name = 'photos'
-    paginate_by = 10 
+    paginate_by = 10
 
+    def get_queryset(self):
+        queryset = super().get_queryset().order_by('-uploaded_at')
+        tag_name = self.request.GET.get('tag')
+        if tag_name:
+            tag = get_object_or_404(Tag, name=tag_name)
+            queryset = queryset.filter(tags=tag)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        return context
+
+class PhotoDetailView(DetailView):
+    model = Photo
+    template_name = 'photos/photo_detail.html'
+    context_object_name = 'photo'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        photo = self.get_object()
+        if self.request.user.is_authenticated:
+            context['user_has_liked'] = Like.objects.filter(photo=photo, user=self.request.user).exists()
+        return context
+
+@login_required
+def like_photo(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    if not Like.objects.filter(photo=photo, user=request.user).exists():
+        Like.objects.create(photo=photo, user=request.user)
+    return redirect('photo_detail', pk=pk)
+
+@login_required
+def unlike_photo(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    Like.objects.filter(photo=photo, user=request.user).delete()
+    return redirect('photo_detail', pk=pk)
